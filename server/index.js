5cea7f10dd1a99992d271ba578197a661926fad1
PCDotFan <pc@edu.ax>
2018-04-06 00:03:07 +0800

    Koa: migrating

--- server/index.js
+++ server/index.js
@@ -1,16 +1,33 @@
+import path from 'path'
+import Koa from 'koa'
+import KoaStatic from 'koa-static'
+import bodyParser from 'koa-bodyparser'
+import Router from 'koa-router'
+import cors from '@koa/cors'
 
-const Koa = require('koa')
+import route from './routes'
+
 const { Nuxt, Builder } = require('nuxt')
 
 const app = new Koa()
 const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
+const router = new Router()
+
+app
+  .use(cors())
+  .use(bodyParser())
+  .use(KoaStatic('.'))
+router.use('', route.routes())
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
 
 // Import and Set Nuxt.js options
 let config = require('../nuxt.config.js')
 config.dev = !(app.env === 'production')
 
-async function start() {
+async function start () {
   // Instantiate nuxt.js
   const nuxt = new Nuxt(config)
 
---
+++ server/routes/index.js
@@ -0,0 +1,14 @@
+import Router from 'koa-router'
+// import config from '../config'
+// import db from '../models'
+// import checkToken from '../middlewares/check-token'
+
+const router = new Router({
+  // prefix: config.app.routerBaseApi
+})
+
+// const user = require('../controllers/user')
+// const tag = require('../controllers/tag')
+// const article = require('../controllers/article')
+
+export default router
