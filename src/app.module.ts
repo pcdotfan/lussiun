280355c61a5e65a5edb44e6504fc2ec1eba12569
PCDotFan <pc@edu.ax>
2018-06-03 23:37:03 +0800

    dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoriesModule } from './categories/categories.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), CategoriesModule, UsersModule],
+  imports: [TypeOrmModule.forRoot(), CategoriesModule, UsersModule, ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,16 +2,20 @@ import * as jwt from 'jsonwebtoken';
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtPayload } from './interfaces/jwt-payload.interface';
+import { ConfigService } from '../config/config.service';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly usersService: UsersService) { }
+    constructor(
+        private readonly usersService: UsersService,
+        private config: ConfigService,
+    ) { }
 
     async createToken(id: number) {
         const user: JwtPayload = { id };
         return jwt.sign(
             user,
-            'vy1C%iZ8W+`]X9#vQl}:/78ul:,CjXcV5{%b-W|_Nty$tGbDnaRGRpJ]e_MkR+O0',
+            this.config.get('JWT_SECRET_KEY'),
             { expiresIn: 3600 });
     }
 
