2773eb6dc11cfb8f463758d77d002ada8f38ea21
PCDotFan <i@oqo.moe>
2018-10-03 21:03:27 -0400

    fix a jwt bug

--- src/jwt.guard.ts
+++ src/jwt.guard.ts
@@ -13,8 +13,7 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
 
     handleRequest(err, user, info) {
         if (err || !user) {
-            return {};
-            // throw err || new UnauthorizedException();
+            throw err || new UnauthorizedException();
         }
         return user;
     }
